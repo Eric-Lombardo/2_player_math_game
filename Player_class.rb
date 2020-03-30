ebc353f36fe6099a76eae6bfd88cf2b1336aacba
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-30 10:59:28 -0400

    add is_player_dead? method to Player

--- Player_class.rb
+++ Player_class.rb
@@ -10,14 +10,22 @@ class Player
     self.life_points -= 1
   end
 
+  def is_player_dead?
+    self.life_points == 0 ? true : false
+  end
 end
 
 
 tron = Player.new("Tron")
 puts tron.life_points
 tron.remove_life_point
+puts tron.is_player_dead?
+puts "==========================="
+puts tron.life_points
+tron.remove_life_point
+puts tron.is_player_dead?
+puts "==========================="
 puts tron.life_points
 tron.remove_life_point
 puts tron.life_points
-tron.remove_life_point
-puts tron.life_points
+puts tron.is_player_dead?
