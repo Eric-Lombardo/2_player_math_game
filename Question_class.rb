f1d4170ab7448031eb877cccb867cc4cb489852f
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-30 13:14:47 -0400

    switch current_player with every new question asked

--- Question_class.rb
+++ Question_class.rb
@@ -1,17 +1,17 @@
 class Question
   attr_accessor :turn
 
-  def initialize(player1, player2)
-    @turn = Turn.new(player1, player2)
+  def initialize(turn)
+    @turn = turn
   end
 
   def math_problem
     num1 = rand(21).to_i
     num2 = rand(21).to_i
     answer = num1 + num2
-    puts "#{turn.current_player}: What is #{num1} + #{num2}?"
+    puts "#{turn.current_player.name}: What is #{num1} + #{num2}?"
     user_answer = gets.chomp.to_i
-    puts user_answer == answer ? "#{turn.current_player}: Yes! That's right!" : "#{turn.current_player}: uhh ... not really"
+    puts user_answer == answer ? "#{turn.current_player.name}: Yes! That's right!" : "#{turn.current_player.name}: uhh ... not really"
     return user_answer == answer ? true : false
   end
 end
