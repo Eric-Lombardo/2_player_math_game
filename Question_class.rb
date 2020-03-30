370186ccc80d264d0ecc3391a2b55ee5aaaa6dc6
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-30 10:28:03 -0400

    ask for user input to math problem

--- Question_class.rb
+++ Question_class.rb
@@ -2,7 +2,10 @@ class Question
   def math_problem
     num1 = rand(21).to_i
     num2 = rand(21).to_i
-    "What is #{num1} + #{num2}?"
+    answer = num1 + num2
+    puts "What is #{num1} + #{num2}?"
+    user_answer = gets.chomp.to_i
+    user_answer == answer ? "Yes! That's right!" : "uhh ... not really"
   end
 end
 
